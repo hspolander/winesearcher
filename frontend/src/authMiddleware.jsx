b20307a5082f594a9c43d6370493a8d0f1ba1b0d
Henrik Spolander <henrik@spolander.nu>
2018-09-16 08:38:49 +0200

    Fix bug where loginwindow didnt show up correct when session was dead

--- frontend/src/authMiddleware.jsx
+++ frontend/src/authMiddleware.jsx
@@ -3,15 +3,11 @@ import Cookies from 'js-cookie';
 export const authMiddleware = store => next => (action) => {
   try {
     if (action.payload && action.type.indexOf('@@redux') === -1) {
-      console.log('action.payload');
-      console.log(action.payload);
       if (action.payload && action.payload.session === 'nosession') {
         store.getState().loginReducer.isAuthenticated = false;
       } else if (action.payload && action.payload.session === 'nosessionRedirect') {
         Cookies.remove('WINE_UUID');
       }
-    console.log('store.getState().loginReducer');
-    console.log(store.getState().loginReducer);
     }
     next(action);
   } catch(e) {
